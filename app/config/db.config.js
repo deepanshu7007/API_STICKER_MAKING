117a4885c60f01d874b7c3a2debb952c6e454bfa
deepanshu7007 <deepanshuchaudhary@jcsoftwaresolution.com>
2024-05-04 15:59:09 +0530

    feat:Added some config changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -9,6 +9,7 @@ const { PORT, DB_PORT, HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 // assert(HOST, 'HOST is required');
 
 module.exports = {
+  url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${HOST}/${DB_NAME}`, //process.env.MONGODB_CONNECT_URIprocess.env.MONGODB_CONNECT_URI
   // url: `mongodb://${DB_USER}:${DB_PASSWORD}@${HOST}:${DB_PORT}/${DB_NAME}`, //process.env.MONGODB_CONNECT_URIprocess.env.MONGODB_CONNECT_URI
-  url: `mongodb://${HOST}:${DB_PORT}/${DB_NAME}`, //process.env.MONGODB_CONNECT_URIprocess.env.MONGODB_CONNECT_URI
+  // url: `mongodb://${HOST}:${DB_PORT}/${DB_NAME}`, //process.env.MONGODB_CONNECT_URIprocess.env.MONGODB_CONNECT_URI
 };

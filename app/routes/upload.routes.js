ab0dda1460129b87308399bbac6d5c4cf0e42787
deepanshu7007 <deepanshuchaudhary@jcsoftwaresolution.com>
2024-05-14 22:57:58 +0530

    update

--- app/routes/upload.routes.js
+++ app/routes/upload.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 var multer = require('multer');
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/img');
+    cb(null, './public/');
   },
   filename: (req, file, cb) => {
    // console.log(file);

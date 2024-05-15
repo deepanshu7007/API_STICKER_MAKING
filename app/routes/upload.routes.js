160055de455a0ef5a6de3366455bf967231be817
deepanshu7007 <deepanshuchaudhary@jcsoftwaresolution.com>
2024-05-15 07:26:14 +0530

    fix:added a new package file for cloundinary

--- app/routes/upload.routes.js
+++ app/routes/upload.routes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 /**Image upload using multer */
+const cloudinary = require('cloudinary').v2; // Import Cloudinary SDK
 var multer = require('multer');
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './public/img');
   },
   filename: (req, file, cb) => {
-   console.log(file);
     var filetype = '';
     if (file.mimetype === 'image/gif') {
       filetype = 'gif';
@@ -22,7 +22,14 @@ var storage = multer.diskStorage({
     cb(null, 'image-' + Date.now() + '.' + filetype);
   },
 });
+
 var upload = multer({ storage: storage });
+
+cloudinary.config({ 
+  cloud_name: 'dimpjogcr', 
+  api_key: '577184584142339', 
+  api_secret: 'yd-2vMTFJsrdIpe2nqeIg6vCRqI' 
+});
 
 var fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -35,13 +42,14 @@ var fileStorage = multer.diskStorage({
     cb(null, 'document-' + Date.now() + `.${ext}` );
   },
 });
+
 var uploadJson = multer({ storage: fileStorage });
 
 
 // module.exports = (app) => {
   // var router = require('express').Router();
 
-  router.post('/image',upload.single('file'),function (req, res, next) {
+ router.post('/image',async function (req, res) {
     try{
     if (!req.file) {
       return res.status(400).json({
@@ -49,12 +57,15 @@ var uploadJson = multer({ storage: fileStorage });
         error: { code: 400, message: 'Please upload a valid file.' },
       });
     }
+
+    const result = await cloudinary.uploader.upload(req.file.path);
+    // console.log(result,"=======================");
     return res.status(200).json({
           success: true,
           message: "Image upload successfully",
           data:{
-            filePath: 'img/' + req.file.filename,
-      fileName: req.file.filename,
+            url: result.url,
+      fileName: result.original_filename,
           }
         });
     // return res.send('Image uploaded successfully!',req.file.filename);
